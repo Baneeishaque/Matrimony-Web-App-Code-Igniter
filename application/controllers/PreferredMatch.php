f769b104f99fce599d8886fdc25b8354a55b57ed
Banee Ishaque K <baneeishaque@gmail.com>
2021-06-16 15:28:49 +0530

    Composer fix

--- application/controllers/PreferredMatch.php
+++ application/controllers/PreferredMatch.php
@@ -1,6 +1,8 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
+require_once 'vendor/autoload.php';
+
 class PreferredMatch extends CI_Controller
 {
 	function __construct()
